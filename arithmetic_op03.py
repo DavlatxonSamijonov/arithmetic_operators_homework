6ba2d78b50019f50cbfe91033c5717aca367ea1c
Dalixan <samijonovdalixan@gmail.com>
2023-10-20 01:22:15 +0500

    uyga vazifa 3

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,7 +1,7 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number = 75
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+n1 = number%10
 #Print the value of the 'answer'
-
-
+n2 = number//10
+print(n1, n2,)
