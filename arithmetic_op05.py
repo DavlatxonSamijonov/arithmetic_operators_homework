3381f78eae5f449894e49c0515a0321ccf378f82
Dalixan <samijonovdalixan@gmail.com>
2023-10-20 10:21:46 +0500

    uyga vazifa 5

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,7 +1,7 @@
 #Create a variable called 'number' with data type int.
-number = 25
+number = 7
 #Create a variable called 'answer' and assign the remainder of the division of number by 3 to it.
-n1 = number%10 # 5 
+answer = number%3
 #Print 'answer'.
-n2 = number/3
-print(n1 / n2)
+
+print(answer)
